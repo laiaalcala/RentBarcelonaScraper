bf93a60d9822488aed1cca36b7d99f26d0d77580
Fernando <telmo_ferc@hotmail.com>
2023-11-05 13:03:31 +0100

    create and process csv file

--- RentBarcelonaScraper/spiders/features_spider.py
+++ RentBarcelonaScraper/spiders/features_spider.py
@@ -3,6 +3,7 @@ import re
 from RentBarcelonaScraper.items import RentBarcelonaItem
 from scrapy.http import TextResponse
 import json
+import csv
 
 class FeatureSpider(scrapy.Spider):
 	name= 'features' 
@@ -18,20 +19,22 @@ class FeatureSpider(scrapy.Spider):
 			# Crear un objeto de respuesta a partir de la cadena de contenido
 			container_response = TextResponse(body=container, url=response.url, encoding='utf-8')
 			price = container_response.css(".ad-preview__price::text").extract_first()
-			bathrooms = container_response.css(".p-sm:nth-child(1)::text").extract_first()
-			rooms = container_response.css(".p-sm:nth-child(2)::text").extract_first()
-			size = container_response.css(".p-sm:nth-child(3)::text").extract_first()
-			characteristics = container_response.css(".p-sm:nth-child(4)::text").extract_first()
-			image = container_response.css("img::attr(src)").extract_first()
+			rooms = container_response.css(".ad-preview__info .ad-preview__section:nth-child(3) .p-sm:nth-child(1)::text").extract_first()
+			bathrooms = container_response.css(".ad-preview__info .ad-preview__section:nth-child(3) .p-sm:nth-child(2)::text").extract_first()
+			size = container_response.css(".ad-preview__info .ad-preview__section:nth-child(3) .p-sm:nth-child(3)::text").extract_first()
+			characteristics = container_response.css(".ad-preview__info .ad-preview__section:nth-child(3) .p-sm:nth-child(4)::text").extract_first()
+			image = container_response.css(".carousel__main-photo img::attr(src)").extract_first()
+			address = container_response.css(".p-sm:nth-child(2)::text").extract_first()
 			#Objeto con la información correspondiente
 			mi_lista = {
+				"address": address,
 				"price": price,
 				"bathrooms": bathrooms,
 				"size": size,
 				"rooms": rooms,
 				"characteristics": characteristics,
 				"image": image,
-				"next_page": next_page
+				"next_page": next_page,
 			}
 			self.data.append(mi_lista)
 
@@ -43,3 +46,19 @@ class FeatureSpider(scrapy.Spider):
 	# Escribir el objeto JSON en un archivo externo
 		with open('result.json', 'w') as json_file:
 			json.dump(self.data, json_file, indent=2)
+
+		# Escritura de los resultados en archivo csv
+		# Obrir un arxiu CSV en mode d'escriptura
+		with open('result.csv', 'w', newline='') as archivo_csv:
+			# Encabezado del csv basado en los keys del json
+			encabezados = self.data[0].keys()
+
+			# Crea un objeto csv.DictWriter
+			escritor = csv.DictWriter(archivo_csv, fieldnames=encabezados)
+
+			# Escritura de los encabezados
+			escritor.writeheader()
+
+			# Escritura de los datos en el csv
+			for fila in self.data:
+				escritor.writerow(fila)
